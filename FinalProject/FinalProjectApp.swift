96a1dbe3d1786d0a5db6e227a72a3af0f0fb71fb
Dylan Nelson <s0512819@student.rockvalleycollege.edu>
2022-10-16 19:16:15 -0500

    Initial Commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by user223537 on 10/16/22.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
